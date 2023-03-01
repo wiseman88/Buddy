4929910748361891efe0e669521d439dd0f67f2b
wiseman88 <wiseadam02@gmail.com>
2023-03-01 20:26:09 +0100

    feat: add dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
+import useColorMode from "@/hooks/useColorMode";
 import { useState } from "react";
 
 export const Header = () => {
   const [display, setDisplay] = useState(false);
+  const [colorMode, setColorMode] = useColorMode();
+
+  const toggleColor = () => {
+    setColorMode(colorMode === "light" ? "dark" : "light");
+  };
 
   return (
     <header
-      className={`fixed w-full bg-white ${
+      className={`fixed w-full bg-white dark:bg-slate-900 dark:text-slate-100 ${
         !display ? "border-b border-gray-200" : ""
       }`}
     >
@@ -13,8 +19,8 @@ export const Header = () => {
         <a className="text-xl sm:text-lg" href="">
           CSS-Buddy
         </a>
-        <nav>
-          <ul className="hidden sm:flex gap-8 uppercase text-sm">
+        <nav className="flex-none">
+          <ul className="hidden sm:flex items-center gap-8 uppercase text-sm">
             <li>
               <a className="hover:text-blue-600" href="">
                 about
@@ -29,6 +35,43 @@ export const Header = () => {
               <a className="hover:text-blue-600" href="">
                 templates
               </a>
+            </li>
+            <li className="flex items-center">
+              <button onClick={toggleColor}>
+                {colorMode === "light" ? (
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={1.5}
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={1.5}
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                    />
+                  </svg>
+                )}
+              </button>
             </li>
           </ul>
         </nav>
