f4c5b0d2cde2df855fec1aafdb38e884f240652f
wiseman88 <wiseadam02@gmail.com>
2023-03-07 16:50:04 +0100

    sidebar link

--- src/components/main/sidebar/NavLink.jsx
+++ src/components/main/sidebar/NavLink.jsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const NavLink = (props) => {
   return (
     <li key={props.key}>
-      <a
-        href=""
+      <Link
         className="block pl-3 border-l text-slate-400 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 hover:border-blue-500 py-2 uppercase text-xs"
+        href={`${props.link.id}`}
       >
         {props.link.title}
-      </a>
+      </Link>
     </li>
   );
 };
