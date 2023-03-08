f21faf7e51307bab59e3b534ee3f2fba11e1e4d0
wiseman88 <wiseadam02@gmail.com>
2023-03-08 18:19:00 +0100

    fix: navlink key undefined error

--- src/components/main/sidebar/NavLink.jsx
+++ src/components/main/sidebar/NavLink.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-const NavLink = (props) => {
+const NavLink = (prop, key) => {
   return (
-    <li key={props.key}>
+    <li key={key}>
       <Link
         className="block pl-3 border-l text-slate-400 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 hover:border-blue-500 py-2 uppercase text-xs"
-        href={`${props.link.id}`}
+        href={`${prop.link.id}`}
       >
-        {props.link.title}
+        {prop.link.title}
       </Link>
     </li>
   );
