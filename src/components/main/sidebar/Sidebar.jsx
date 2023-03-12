ffe003b79914fa62fd75545e0873f12ed73dadde
wiseman88 <wiseadam02@gmail.com>
2023-03-12 19:46:43 +0100

    style: dark mode sidebar

--- src/components/main/sidebar/Sidebar.jsx
+++ src/components/main/sidebar/Sidebar.jsx
@@ -16,13 +16,15 @@ const Sidebar = () => {
     <aside
       className={`${
         sidebar ? "hidden" : ""
-      } md:block fixed bottom-0 w-full md:w-[12rem] overflow-y-auto bg-white`}
+      } md:block fixed bottom-0 w-full md:w-[12rem] overflow-y-auto bg-white dark:bg-black text-slate-200`}
       style={{ top: height }}
     >
       <nav>
         <ul className="pt-4">
           <li>
-            <h5 className="font-bold mb-2 dark:text-white">Components</h5>
+            <h5 className="font-bold mb-2 text-slate-900 dark:text-white">
+              Components
+            </h5>
             <ul>
               {sidebarLinks.map((link, key) => (
                 <NavLink link={link} key={key} />
